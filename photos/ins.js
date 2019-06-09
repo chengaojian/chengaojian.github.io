f91ee0d9278f4ea1eeadadacb7e8ac287c815700
chengaojian <chengaojian@haixun360.com>
2019-06-09 17:48:10 +0800

    Site updated: 2019-06-09 17:48:10

--- photos/ins.js
+++ photos/ins.js
@@ -116,7 +116,7 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/chengaojian/Blog-Back-Up/tree/master/min_photos/' + data.link[i] + '.min.jpg';
+          var minSrc = 'https://github.com/chengaojian/Blog-Back-Up/tree/master/min_photos/' + data.link[i];
           var src = 'https://github.com/chengaojian/Blog-Back-Up/blob/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src;
